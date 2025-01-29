0165522000ebf5ee9ea2da88d2f3b4a930bfcfe9
Ilya Gerilovich <gerilovich.ip@gmail.com>
2025-01-29 20:31:00 +0400

    Update state.py

--- state.py
+++ state.py
@@ -1,22 +1,22 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class User(StatesGroup):
-    name = State()  # Состояние для имени
-    weight = State()  # Состояние для веса
-    height = State()  # Состояние для роста
-    age = State()  # Состояние для возраста
-    activity_level = State()  # Состояние для уровня активности
-    city = State()  # Состояние для города
-    calorie_goal = State()  # Состояние для цели калорий
-    logged_water = State() # Состояние для выпитой воды
-    logged_calories = State() # Состояние для полученных калорий
-    burned_calories = State() # Состояние для сожженных калорий
+    name = State()  
+    weight = State()  
+    height = State()  
+    age = State()  
+    activity_level = State()  
+    city = State()  
+    calorie_goal = State()  
+    logged_water = State() 
+    logged_calories = State() 
+    burned_calories = State() 
 
 class Food(StatesGroup):
-    product = State() # Состояние для названия продукта
-    gram = State() # Состояние для съеденных грамм продукта
+    product = State() 
+    gram = State()
 
 class Workout(StatesGroup):
-    name_w = State() # Состояние для названия тренировки
-    time = State() # Состояние для времени тренировки
-    water_intake_w = State() # Состояние для дополнительной воды
+    name_w = State() 
+    time = State() 
+    water_intake_w = State() 
